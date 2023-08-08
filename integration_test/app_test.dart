91219ef2431d2827fd0934ab603b8f1343212bc3
neil.warner <neil.warner@everbridge.com>
2023-08-08 17:22:53 -0500

    works, test passes

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,4 +1,5 @@
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:tsf_pat_dp/main.dart';
@@ -29,6 +30,43 @@ void main() {
 
       // Verify the counter increments by 1.
       expect(find.text('1'), findsOneWidget);
+      await tester.tap(find.text('Device Preview'));
+      await tester.pumpAndSettle(const Duration(milliseconds: 200));
+
+      await tester.drag(find.text('Frame visibility'), const Offset(0, -400));
+      await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+      await tester.drag(find.text('Accessible navigation'), const Offset(0, -200));
+      await tester.pumpAndSettle(const Duration(milliseconds: 200));
+
+      await tester.drag(find.text('Bold text'), const Offset(0, -200));
+      await tester.pumpAndSettle(const Duration(milliseconds: 200));
+
+      expect(find.text('Text scaling factor'), findsOneWidget);
+
+      /*
+      await tester.dragUntilVisible(
+          find.text('Locale'), find.text('Text Scaling factor', skipOffstage: false), Offset(0, -100));
+     */
+      expect(find.text('1.0'), findsOneWidget);
+
+      await tester.slideToValue(find.byType(Slider, skipOffstage: false), 65);
+      await tester.pumpAndSettle(const Duration(seconds: 3));
+
+      expect(find.text('2.0'), findsOneWidget);
+      await tester.pumpAndSettle(const Duration(seconds: 3));
+
+      // could confirm the you have pressed not visibile before scale 2
+      // and visible adter
     });
   });
+}
+
+extension SlideTo on WidgetTester {
+  Future<void> slideToValue(Finder slider, double value, {double paddingOffset = 24.0}) async {
+    final zeroPoint = this.getTopLeft(slider) + Offset(paddingOffset, this.getSize(slider).height / 2);
+    final totalWidth = this.getSize(slider).width - (2 * paddingOffset);
+    final calculatdOffset = value * (totalWidth / 100);
+    await this.dragFrom(zeroPoint, Offset(calculatdOffset, 0));
+  }
 }
