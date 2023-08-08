3411e6d0f95e131bcf28c52f43b6be3800996b28
neil.warner <neil.warner@everbridge.com>
2023-08-08 14:12:51 -0500

    Initial commit hello world plus device preview plus int test

---
+++ integration_test/app_test.dart
@@ -0,0 +1,34 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:tsf_pat_dp/main.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter', (tester) async {
+      // Load app widget.
+      // or main as app.main()
+      await tester.pumpWidget(DevicePreview(
+        enabled: true,
+        builder: (context) => MyApp(), // Wrap your app
+      ));
+
+      // Verify the counter starts at 0.
+      expect(find.text('0'), findsOneWidget);
+
+      // Finds the floating action button to tap on.
+      final Finder fab = find.byTooltip('Increment');
+
+      // Emulate a tap on the floating action button.
+      await tester.tap(fab);
+
+      // Trigger a frame.
+      await tester.pumpAndSettle();
+
+      // Verify the counter increments by 1.
+      expect(find.text('1'), findsOneWidget);
+    });
+  });
+}
